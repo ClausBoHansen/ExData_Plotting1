a69486f6da28cc67b72ee683fd1d7b220e512914
Claus Bo Hansen <sbo@trolling.dk>
2017-12-29 14:57:04 +0100

    Add updated plot1.R

--- plot1.R
+++ plot1.R
@@ -15,3 +15,11 @@ consumption <- read.table(datafile, sep = ";", header = TRUE, colClasses = c(rep
 
 # Subset to only 2007-02-01 and 2007-02-02
 consumption <- consumption[which(grepl("^(1|2)/2/2007$", consumption$Date)),]
+
+# Combine Data and Time, then convert POSIXct
+consumption$timestamp <- as.POSIXct(paste(consumption$Date,consumption$Time), format = "%e/%m/%Y %H:%M:%S")
+
+# Create plot
+png(file = "plot1.png", width = 480, height = 480)
+hist(consumption$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
